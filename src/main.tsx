d7c2851de464140257c828768eec9c93a2ec7f73
badhope <badhope@users.noreply.github.com>
2026-05-10 10:31:15 +0000

    feat: 当前网站情况
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -32,11 +32,9 @@ setTimeout(() => {
 }, 3000)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <Router>
-        <App />
-      </Router>
-    </ErrorBoundary>
-  </React.StrictMode>,
+  <ErrorBoundary>
+    <Router>
+      <App />
+    </Router>
+  </ErrorBoundary>
 )
